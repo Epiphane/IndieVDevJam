ab9c231e8b3fecd3ec7517be3355bbd63f7d4b9e
Thomas Steinke <thomas.steinke@weebly.com>
2015-07-19 01:40:08 -0700

    Made it look older

--- src/sound_manager.js
+++ src/sound_manager.js
@@ -1,18 +1,27 @@
 var fader = document.getElementById('fader');
 
+// Set defaults
 fader.value = localStorage.getItem('volume') || 60;
 buzz.defaults.volume = fader.value;
 
+function setVolume(val) {
+    localStorage.setItem('volume', val);
+    buzz.defaults.volume = fader.value = val;
+}
+
+// Handle volume changes
 fader.addEventListener('change', function(e) {
-    localStorage.setItem('volume', e.target.value);
-    buzz.defaults.volume = fader.value;
+    setVolume(parseInt(e.target.value));
 });
+document.getElementById('mute').onclick = function() {
+    setVolume(0);
+};
 
 var newBuzzSound = (function() {
     var globalSounds = [];
 
     // "Fix" the buzz volume management
-    
+
 
     return function(src, options) {
         var sound = new buzz.sound(src, options);
