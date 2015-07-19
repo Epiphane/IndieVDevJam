8a8679cba2a7d42f940d0ce833598582e117f05f
Thomas Steinke <thomas.steinke@weebly.com>
2015-07-19 02:22:45 -0700

    UI

--- src/sound_manager.js
+++ src/sound_manager.js
@@ -1,30 +1,36 @@
-var fader = document.getElementById('fader');
-
-// Set defaults
-fader.value = localStorage.getItem('volume') || 60;
-buzz.defaults.volume = fader.value;
-
-function setVolume(val) {
-    localStorage.setItem('volume', val);
-    buzz.defaults.volume = fader.value = val;
-}
-
-// Handle volume changes
-fader.addEventListener('change', function(e) {
-    setVolume(parseInt(e.target.value));
-});
-document.getElementById('mute').onclick = function() {
-    setVolume(0);
-};
-
 var newBuzzSound = (function() {
     var globalSounds = [];
 
-    // "Fix" the buzz volume management
+    // Handle volume changes
+    var muter = document.getElementById('mute');
+    var gameMuted = localStorage.getItem('mute') || false;
 
+    // Set the graphic
+    var i = muter.children[0];
+    if (gameMuted)
+        i.setAttribute('class', 'fa fa-volume-up');
+    else
+        i.setAttribute('class', 'fa fa-volume-off');
+
+    muter.onclick = function() {
+        gameMuted = !gameMuted;
+
+        if (gameMuted)
+            i.setAttribute('class', 'fa fa-volume-up');
+        else
+            i.setAttribute('class', 'fa fa-volume-off');
+
+        localStorage.setItem('mute', gameMuted);
+
+        // Change all globalSounds to be muted or not muted
+        // ...
+    };
 
     return function(src, options) {
         var sound = new buzz.sound(src, options);
+
+        // if gameMuted then mute the sound immediately
+        // ...
 
         globalSounds.push(sound);
 
