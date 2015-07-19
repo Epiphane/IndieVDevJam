e56f06decc63716417e1451d7f1cc3f48a75fe04
Thomas Steinke <thomas.steinke@weebly.com>
2015-07-19 01:00:35 -0700

    Crap I broke everything

--- src/components/booklet.js
+++ src/components/booklet.js
@@ -41,63 +41,6 @@ Juicy.Component.create('Booklet', {
         });
     },
 
-<<<<<<< HEAD
-   update: function(dt, input) {
-      var tileManager = this.entity.scene.tileManager.getComponent('LevelTiles');
-
-      var transform = this.entity.transform;
-
-      var ray;
-      if (this.dx > 0) {
-         // Moving right
-         var top = tileManager.raycast(transform.position.x + transform.width, transform.position.y, this.dx * dt, 0);
-         var bot = tileManager.raycast(transform.position.x + transform.width, transform.position.y + transform.height, this.dx * dt, 0);
-
-         if (Math.abs(top.dx) > Math.abs(bot.dx)) ray = bot;
-         else ray = top;
-      }
-      else {
-         // Moving left
-         var top = tileManager.raycast(transform.position.x, transform.position.y, this.dx * dt, 0);
-         var bot = tileManager.raycast(transform.position.x, transform.position.y + transform.height, this.dx * dt, 0);
-      
-         if (Math.abs(top.dx) > Math.abs(bot.dx)) ray = bot;
-         else ray = top;
-      }
-      transform.position.x += ray.dx;
-
-      this.life -= dt;
-      if (Math.abs(ray.dx) < 0.1 || this.life < 0) {
-         if (this.dx < 0) {
-            this.deathParticles(this.entity.transform.position.x);//-ray.dx);   
-         }
-         else {
-            this.deathParticles(this.entity.transform.position.x + 1);      
-         }
-            
-         this.entity.dead = true;
-         return;
-      }
-
-      var enemies = this.entity.scene.enemies;
-      for (var i = 0; i < enemies.length; i ++) {
-         if (this.entity.transform.testCollision(enemies[i].transform)) {
-            enemies[i].getComponent('Enemy').health -= 30;
-            this.hitSound.play();
-            this.entity.dead = true;
-            return;
-         }
-      }
-
-      var objects = this.entity.scene.objects;
-      for (var i = 0; i < objects.length; i ++) {
-         if (this.entity !== objects[i] && this.entity.transform.testCollision(objects[i].transform)) {
-            if (objects[i].getComponent('Destructible')) {
-                objects[i].getComponent('Destructible').health -= 30;
-                
-                this.deathParticles(objects[i].transform.width);
-                this.hitSound.play();
-=======
     update: function(dt, input) {
         var tileManager = this.entity.scene.tileManager.getComponent('LevelTiles');
 
@@ -140,7 +83,6 @@ Juicy.Component.create('Booklet', {
             if (this.entity.transform.testCollision(enemies[i].transform)) {
                 enemies[i].getComponent('Enemy').health -= 30;
 
->>>>>>> Fixed the book coloring and made shooting higher
                 this.entity.dead = true;
                 return;
             }
